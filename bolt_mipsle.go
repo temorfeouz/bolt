32bbac5913e17ebdca3f08ffe5d8318a0f6f8c08
klychka <klychka@rozetka.com.ua>
2021-10-11 18:59:08 +0300

    add mips compability

--- bolt_mipsle.go
+++ bolt_mipsle.go
@@ -1,10 +1,5 @@
 package bolt
 
-import (
-	"syscall"
-	"unsafe"
-)
-
 // maxMapSize represents the largest mmap size supported by Bolt.
 const maxMapSize = 0x40000000 // 1GB
 
@@ -13,12 +8,3 @@ const maxAllocSize = 0xFFFFFFF
 
 // brokenUnaligned Are unaligned load/stores broken on this arch?
 var brokenUnaligned = false
-
-// NOTE: This function is copied from stdlib because it is not available on darwin.
-func madvise(b []byte, advice int) (err error) {
-	_, _, e1 := syscall.Syscall(syscall.SYS_MADVISE, uintptr(unsafe.Pointer(&b[0])), uintptr(len(b)), uintptr(advice))
-	if e1 != 0 {
-		err = e1
-	}
-	return
-}
